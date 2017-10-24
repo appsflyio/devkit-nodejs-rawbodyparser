6c17651c5937b094c3a4baeb08e280096c444504
Mohit Bhansali <mohit@appsfly.io>
2017-10-24 17:07:22 +0530

    Added try catch block

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = function(bodyParser){
     return function(req, res, next){
-        bodyParser.raw({type:"application/json"})(req, res, function () {
-            req.rawBody = req.body;
-            req.body = JSON.parse(req.rawBody.toString("UTF-8"));
-            next();
-        });
+        try {
+            bodyParser.raw({type:"application/json"})(req, res, function () {
+                req.rawBody = req.body;
+                req.body = JSON.parse(req.rawBody.toString("UTF-8"));
+                next();
+            });
+        } catch(error) {
+            next(error);
+        }
     }
 }
